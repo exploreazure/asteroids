5364b37b23228c887801c4baf1459bd2979d3231
Barry Sharpen <barrysharpen@noreply.github.com>
2025-09-03 16:23:13 +0100

    Add player and circle shape classes with movement and drawing functionality

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 import pygame
 
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -11,8 +12,16 @@ def main():
     print(f'Screen height: {SCREEN_HEIGHT}')
     # pygame init
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    # Create Groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    # Assign groups to classes
+    Player.containers = (updatable, drawable)
     # Create a new GUI Window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
     # create loop
     loop_flag = True
     while (loop_flag == True):
@@ -20,6 +29,11 @@ def main():
          if event.type == pygame.QUIT:
               return
        screen.fill('black')
+       time_passed = clock.tick(60)
+       dt = time_passed / 1000
+       updatable.update(dt)
+       for drawable_group in drawable:
+          drawable_group.draw(screen)
        pygame.display.flip()
 
 if __name__ == "__main__":
